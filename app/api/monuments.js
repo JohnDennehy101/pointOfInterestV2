af9eed97797d3793fc857b259715b804efe94da6
JohnDennehy <johndennehy101@gmail.com>
2021-04-05 20:42:58 +0100

    Added api endpoints with relevant methods for monuments.

---
+++ app/api/monuments.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const Monument = require('../models/monuments');
+const Boom = require("@hapi/boom");
+
+const Monuments = {
+  find: {
+    auth: false,
+    handler: async function (request, h) {
+      const monuments = await Monument.find();
+      return monuments;
+    },
+  },
+  findOne: {
+    auth: false,
+    handler: async function (request, h) {
+      try {
+        const monument = await Monument.findOne({ _id: request.params.id });
+        if (!monument) {
+          return Boom.notFound("No monument with this id");
+        }
+        return monument;
+      } catch (err) {
+        return Boom.notFound("No monument with this id");
+      }
+    },
+  },
+  create: {
+    auth: false,
+    handler: async function (request, h) {
+      const newMonument = new Monument(request.payload);
+      const monument = await newMonument.save();
+      if (monument) {
+        return h.response(monument).code(201);
+      }
+      return Boom.badImplementation("error creating monument");
+    },
+  },
+  deleteAll: {
+    auth: false,
+    handler: async function (request, h) {
+      await Monument.remove({});
+      return { success: true };
+    },
+  },
+  deleteOne: {
+    auth: false,
+    handler: async function(request, h) {
+      const response = await Monument.deleteOne({ _id: request.params.id });
+      if (response.deletedCount == 1) {
+        return { success: true };
+      }
+      return Boom.notFound('id not found');
+    }
+  },
+}
+
+module.exports = Monuments;
--- routes-api.js
+++ routes-api.js
@@ -1,4 +1,5 @@
 const Users = require('./app/api/accounts');
+const Monuments = require('./app/api/monuments');
 
 module.exports = [
   { method: 'GET', path: '/api/users', config: Users.find },
@@ -6,5 +7,10 @@ module.exports = [
   { method: "POST", path: "/api/users", config: Users.create },
   { method: "DELETE", path: "/api/users/{id}", config: Users.deleteOne },
   { method: "DELETE", path: "/api/users", config: Users.deleteAll },
+  { method: 'GET', path: '/api/monuments', config: Monuments.find },
+  { method: 'GET', path: '/api/monuments/{id}', config: Monuments.findOne },
+  { method: "POST", path: "/api/monuments", config: Monuments.create },
+  { method: "DELETE", path: "/api/monuments/{id}", config: Monuments.deleteOne },
+  { method: "DELETE", path: "/api/monuments", config: Monuments.deleteAll },
 
  ];
